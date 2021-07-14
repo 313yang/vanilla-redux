3321058abf6e7fb7eda263ae2577fd426d9035c3
313yang <313yang@naver.com>
2021-07-14 10:15:08 +0900

    install redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("hello");
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const counter = (count = 0, action) => {
+  return count;
+};
+
+const countStore = createStore(counter);
