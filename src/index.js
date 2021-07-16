ac283c28b3bc1613f689aaecd4a3b5acdbe406af
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-07-16 17:08:46 +0900

    vanilla js redux todolist practice-delete Btn

--- src/index.js
+++ src/index.js
@@ -7,27 +7,55 @@ const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE TODO";
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: action.id }];
+      return [{ text: action.text, id: action.id }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter((todo) => todo.id !== action.id);
     default:
       return state;
   }
 };
-
+const addToDo = (text) => {
+  return { type: ADD_TODO, text, id: Date.now() };
+};
+const deleteToDo = (id) => {
+  return { type: DELETE_TODO, id };
+};
 const store = createStore(reducer);
-store.subscribe(() => {
-  return console.log(store.getState());
-});
+
+const dispatcDeleteToDo = (e) => {
+  const id = +e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+};
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    deleteBtn.addEventListener("click", dispatcDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
 
 const handleSubmit = (event) => {
   event.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo, id: Date.now() });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", handleSubmit);
