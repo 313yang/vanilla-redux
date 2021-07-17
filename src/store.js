d25753d4df61adc92d6568bab1a313675f70f46f
313yang <313yang@naver.com>
2021-07-17 19:04:28 +0900

    createAction

--- src/store.js
+++ src/store.js
@@ -1,24 +1,15 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-const DELETE_PAGE = "DELETE_PAGE";
-
-const addToDo = (text) => {
-  return { type: ADD, text, id: Date.now() };
-};
-const deleteToDo = (id) => {
-  return { type: DELETE, id };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: action.id }, ...state];
-    case DELETE:
-      return state.filter((todo) => todo.id !== action.id);
-    case DELETE_PAGE:
-      return [];
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((todo) => todo.id !== action.payload);
     default:
       return state;
   }
