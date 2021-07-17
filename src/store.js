0b0eb0a1c878e676e7ebcf501d210ecea9178482
313yang <313yang@naver.com>
2021-07-17 19:51:12 +0900

    useing redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,21 +1,18 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((todo) => todo.id !== action.payload);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+    // The toolkit allows state to be 'mutate'. If mutate the state, don't return anything. ! only 'new' state can be return !
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((todo) => todo.id !== action.payload),
+});
+const store = configureStore({ reducer });
 export const actionCreators = {
   addToDo,
   deleteToDo,
